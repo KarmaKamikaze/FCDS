0efd9b1a77ddb675fafd7550ee1a85c0ad83efd3
Nicolai Jørgensen <nicolaijgn@gmail.com>
2021-03-18 10:07:33 +0100

    Pushed weighting attribute to relax function and other minor changes

--- node/PublicResources/js/dijkstra.js
+++ node/PublicResources/js/dijkstra.js
@@ -1,10 +1,6 @@
-// Initialize single source
-// Relax
-// Dijkstra
-// Priority queue (increasing order) (min-heap)
-// (Weight function)
 import { PriorityQueue } from "./queue.js";
 import { initializeSingleSource, relax } from "./pathModules.js";
+// IMPORT CYTOSCOPE
 
 /**
  *
@@ -18,7 +14,7 @@ export function dijkstra(graph, startNode) {
   let queue = new PriorityQueue();
 
   graph.nodes.forEach((element) => {
-    queue.enqueue(element.id, element.distanceOrigin);
+    queue.enqueue(element.id(), element.distanceOrigin);
   });
 
   while (!queue.isEmpty) {
@@ -27,18 +23,33 @@ export function dijkstra(graph, startNode) {
     // For-loop that checks if each edge's source is the observed node.
     graph.edges.forEach((edge) => {
       if (edge.source() === shortestDistance.identifier) {
-        relax(shortestDistance, edge.target());
+        let weight = graph.getElementById(
+          `${shortestDistance.identifier}${edge.target().id()}`
+        ).weight;
+        relax(shortestDistance, edge.target(), weight);
       }
     });
   }
 }
 
+/**
+ *
+ * @param {The graph which contains distances and parents,
+ * which we will use for navigation.} graph
+ * @param {The end goal for which we want to find the shortest path.} endNode
+ */
 export function traceback(graph, endNode) {
   let shortestPath = "";
   let jump = endNode;
 
+  /* While-loop that reiterates through the parents of jump,
+   * creating a list of nodes used to go from startnode to endnode. */
   while (jump.parent !== null && jump.distance !== 0) {
-    shortestPath = jump.id() + " -> " + shortestPath;
+    if (shortestPath === "") {
+      shortestPath = jump.id();
+    } else {
+      shortestPath = jump.id() + " -> " + shortestPath;
+    }
     jump = graph.getElementById(`${jump.parent.identifier}`);
   }
   // Add the start node to the list.
--- node/PublicResources/js/pathModules.js
+++ node/PublicResources/js/pathModules.js
@@ -1,9 +1,18 @@
+/**
+ *
+ * @param {The graph which will be initialized and reset: The starting point will be set to 0,
+ * while every other node will have a distance of Infinity. All parents will be zero.} graph
+ * @param {The source node/origin point.} startNode
+ */
 export function initializeSingleSource(graph, startNode) {
   // Change identifier after cytoscape is implemented
+  /* All nodes are initialized by setting their distance to
+   * the origin/source to infinity and setting their parents to null. */
   graph.nodes.forEach((element) => {
     element.distanceOrigin = Infinity;
     element.parent = null;
   });
+  // The startnode's distance to itself is assigned to 0
   startNode.nodes.distanceOrigin = 0;
 }
 
@@ -13,16 +22,12 @@ export function initializeSingleSource(graph, startNode) {
  * @param {A node adjacent to currentnode.
  * This node is the target of an edge, which has the source of currentnode} adjacentNode
  */
-export function relax(currentNode, adjacentNode) {
-  if (
-    adjacentNode.distance >
-    currentNode.distance + weight(currentNode, adjacentNode)
-  ) {
+export function relax(currentNode, adjacentNode, weight) {
+  if (adjacentNode.distance > currentNode.distance + weight) {
     /* The distance from the source to the adjacent node is updated through addition
      * of the source's distance to the current node
      * and the weight between the current node and the adjacent node */
-    adjacentNode.distance =
-      currentNode.distance + weight(currentNode, adjacentNode);
+    adjacentNode.distance = currentNode.distance + weight;
     /* The parent will retain the path back to the starting points,
      * if combined with all other parents. */
     adjacentNode.parent = currentNode;
