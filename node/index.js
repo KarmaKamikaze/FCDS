737e9b8138e5f329ff458c0b84c88a21bbd712b7
Nicolai Jørgensen <nicolaijgn@gmail.com>
2021-03-27 17:34:27 +0100

    Added static options

--- node/index.js
+++ node/index.js
@@ -7,10 +7,28 @@ const app = express();
 const port = 3000;
 
 // Middleware
-app.use(express.static(path.join(__dirname, "node", "PublicResources")));
+let options = {
+  dotfiles: "ignore", // allow, deny, ignore
+  etag: true,
+  extensions: ["htm", "html", "js", "css", "ico", "cyjs"],
+  index: false, // Disables directory indexing - won't serve a whole folder
+  maxAge: "7d", // Expires after 7 days
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    // Add this to header of all statis responses
+    res.set("x-timestamp", Date.now());
+  },
+};
+
+app.use(
+  express.static(path.join(__dirname, "node", "PublicResources"), options)
+);
 app.use(
   "/node_modules/cytoscape/dist",
-  express.static(path.join(__dirname, "node_modules", "cytoscape", "dist"))
+  express.static(
+    path.join(__dirname, "node_modules", "cytoscape", "dist"),
+    options
+  )
 );
 
 // Routes
